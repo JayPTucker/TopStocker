e8a381966675fb017ee65b6555a082754bf6a5cb
JayPTucker <jayptuckerimportant@gmail.com>
2020-12-11 16:37:17 -0600

    Fixing our server.js file.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// *****************************************************************************
+// *********************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
+// *********************************************************************************
+
+// Dependencies
 // =============================================================
 var express = require("express");
 
@@ -11,26 +11,20 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/item-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
+// Starts the server to begin listening
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
