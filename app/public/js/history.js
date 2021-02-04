bf77af69f261f823112abf73f0bd4a836c856bde
JayPTucker <jaypaultucker@gmail.com>
2021-02-03 19:38:21 -0600

    More CSS Changes #3

--- app/public/js/history.js
+++ app/public/js/history.js
@@ -31,7 +31,7 @@ if (historyTmp == null) {
   
         row.append(`<img class="stock-img" src='stock-photo.jpg' width='150' height='150'>`)
         row.append("<p class='item-number'>Item # " + data[i].item_number + "</p>");
-        row.append(`<div class="col-md-12 stock-div"><span><p>On Hand: ${data[i].quantity}</p><button id='editQty' data-id="${data[i].item_number}">Edit Quantity</button></span></div>`)
+        row.append(`<div class="col-md-12 stock-div"><span><p>Top Stock Qty: ${data[i].quantity}</p><button id='editQty' data-id="${data[i].item_number}">Edit Quantity</button></span></div>`)
         row.append(`<div class="location-div"><span><p>Aisle: ${data[i].aisle_number}, Bay: ${data[i].bay_number}</p></p><button id='editLocation' data-id="${data[i].item_number}">Edit Location</button></span></div>`)
         row.append("<button id='deleteItem' data-id='" + data[i].item_number + "'>Delete Item</button><br>")
         row.append("<p class='creation-date'>Created: " + moment(data[i].createdAt).format("MMMM Do YYYY, h:mm:ss a") + "</p>");
